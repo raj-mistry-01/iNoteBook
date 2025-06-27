9fad211751dbba7eb3527bd263da34eaf6dbdbf8
Makwana Raj <151895608+raj-mistry-01@users.noreply.github.com>
2025-06-27 22:00:55 +0530

    1st

---
+++ Backend/db.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+const mongoose = require("mongoose")
+const mongoURI = process.env.MONGO_URI
+
+const connectToMongo = () => {
+    mongoose.connect(mongoURI)
+    console.log("Connected to mongo");
+}
+
+module.exports  = connectToMongo;
